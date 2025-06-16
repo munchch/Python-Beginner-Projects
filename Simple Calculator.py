6abc646d2f662dbba576a563403cf9f401c31692
Arda Özçelik <72027135+munchch@users.noreply.github.com>
2025-06-16 17:48:15 +0300

    Add files via upload

---
+++ Simple Calculator.py
@@ -0,0 +1,44 @@
+
+        
+while True:
+    welcoming= input("\nWelcome! Please text c to continue or q to quit:")
+    if welcoming.lower() == "q":
+        break
+    elif welcoming.lower() != "c":
+        print("Invalid input please enter either c or q!")
+        continue
+  
+    try:
+        
+        number1= int(input("Please enter the first number: "))
+        number2= int(input("Please enter the second number: "))
+        operation= input("Please enter the math operation (+, -, *, /):")
+        
+        if operation not in ["+", "-", "*", "/"]:
+            print("Invalid operation! Please use +, -, *, or /.")
+            continue
+            
+            
+        if operation == "+":
+            print(str((number1+number2))+ " is the result")
+        
+        elif operation == "-":
+            print(str((number1-number2))+ " is the result")
+            
+        elif operation == "/":
+            if number2== 0:
+                print("Zero cannot be a denominator")
+            else:
+                print(str((number1/number2))+ " is the result")   
+            
+            
+        elif operation == "*":
+            print(str((number1*number2))+ " is the result")
+            
+    except: 
+        print("Please enter viable values")
+    
+
+    
+    
+        
