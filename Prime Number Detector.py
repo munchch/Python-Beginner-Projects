15b271ede061fd7806d25b756fe6626cbaaa80f6
Arda Özçelik <72027135+munchch@users.noreply.github.com>
2025-06-16 17:53:11 +0300

    Update Prime Number Detector.py
    A simple Python script that checks whether a number is prime or not.  
    It uses a basic loop and conditionals.  
    Input: Integer from user  
    Output: True/False + explanation

--- Prime Number Detector.py
+++ Prime Number Detector.py
@@ -1,14 +1,14 @@
 def detector(number):
-    if number <= 1:
+    if number <= 1:  # Excluded numbers smaller than 1 (including one) as not prime number because they are not
         return False
     
-    for i in range (2, number):
+    for i in range (2, number): # An algorithm divides every number from 2 to given number to the given number and testing if there is a residual
         if number % i ==0:
             return False
     else:
         return True
 
-while True:
+while True: # Used while so that algorithm could be in a loop
     number = (input("\nEnter the number you want to check or q to quit:"))
     if number.lower() == "q":
         break
